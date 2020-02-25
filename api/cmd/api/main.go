1dd74b5c919b8c45a6c699403122fd04a7b72bf3
Haardik <hhaardik@edu.uwaterloo.ca>
2020-02-25 15:09:20 -0500

    dockerize angular+golang api

---
+++ api/cmd/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":5000", r)
+}
---
+++ api/go.mod
@@ -0,0 +1,5 @@
+module github.com/haardikk21/algorand-asset-manager/api
+
+go 1.13
+
+require github.com/go-chi/chi v4.0.3+incompatible
