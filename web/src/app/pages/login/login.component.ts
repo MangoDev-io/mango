9b9e4d47e905597f44abb3d724ef6436c52fda28
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-02-27 22:00:11 -0500

    Added bulma, styled login, added login form/route

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }       from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public mnemonic: string;
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  loginClickHandler() {
+    if (!this.mnemonic) {
+      alert('Please enter the mnemonic');
+    } else {
+      console.log(this.mnemonic);
+      this.router.navigate(['/manage']);
+    }
   }
 
 }
