101514316b863918fd6e518abae576c0f4d0fe4a
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-02-27 20:30:46 -0500

    Added login, manage pages + routing

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ManageComponent } from './pages/manage/manage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'manage',
+    component: ManageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
