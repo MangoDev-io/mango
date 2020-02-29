3bea78db1019d9970450e62e444332958f47e55f
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-02-29 17:56:49 -0500

    Added service to token lister + added prettier settings to package.json

--- web/src/app/components/token-lister/token-lister.component.ts
+++ web/src/app/components/token-lister/token-lister.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StateService } from '../../state.service';
 
 @Component({
   selector: 'app-token-lister',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TokenListerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public stateSvc: StateService) { }
 
   ngOnInit(): void {
   }
