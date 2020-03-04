d5580cb7cc4c5bef3e9ce3da9fcb329dce230276
Haardik <hhaardik@edu.uwaterloo.ca>
2020-03-03 22:04:36 -0500

    add jwt to api

--- api/cmd/api/router.go
+++ api/cmd/api/router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/algorand/go-algorand-sdk/client/algod"
 	"github.com/algorand/go-algorand-sdk/client/kmd"
+	"github.com/go-chi/jwtauth"
 	"github.com/haardikk21/algorand-asset-manager/api/cmd/api/data"
 	"github.com/haardikk21/algorand-asset-manager/api/cmd/api/routes"
 
@@ -11,26 +12,38 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Router is the HTTP Router service
 type Router struct {
 	*chi.Mux
 	log   *logrus.Logger
 	db    *data.DatabaseService
 	kmd   *kmd.Client
 	algod *algod.Client
+	jwt   *jwtauth.JWTAuth
 }
 
-// Return a new instance of the Router
-func NewRouterService(logger *logrus.Logger, db *data.DatabaseService, kmd *kmd.Client, algod *algod.Client) *Router {
+// NewRouterService return a new instance of the Router
+func NewRouterService(logger *logrus.Logger, db *data.DatabaseService, kmd *kmd.Client, algod *algod.Client, jwt *jwtauth.JWTAuth) *Router {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger, middleware.RedirectSlashes)
 
-	managerHandler := routes.NewManagerHandler(logger, db, kmd, algod)
+	managerHandler := routes.NewManagerHandler(logger, db, kmd, algod, jwt)
 
-	router.Get("/", managerHandler.GetHello)
-	router.Post("/createAsset", managerHandler.CreateAsset)
-	router.Get("/assets", managerHandler.GetAssets)
+	// JWT Protected Routes
+	router.Group(func(router chi.Router) {
+		router.Use(jwtauth.Verifier(jwt))
+		router.Use(jwtauth.Authenticator)
 
-	service := &Router{router, logger, db, kmd, algod}
+		router.Post("/createAsset", managerHandler.CreateAsset)
+		router.Get("/assets", managerHandler.GetAssets)
+	})
+
+	// Public Routes
+	router.Group(func(router chi.Router) {
+		router.Post("/encryptMnemonic", managerHandler.EncryptMnemonic)
+	})
+
+	service := &Router{router, logger, db, kmd, algod, jwt}
 	return service
 }
