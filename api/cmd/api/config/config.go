8319ac0b478bd3778a23919aaa1d3e38e99d7242
Ryan Ouyang <ryan.ouyang01@gmail.com>
2020-03-06 00:02:58 -0500

    Removed KMD stuff from API config

--- api/cmd/api/config/config.go
+++ api/cmd/api/config/config.go
@@ -3,8 +3,6 @@ package config
 type Config struct {
 	AlgodAddress string `required:"true"`
 	PSToken      string `required:"true"`
-	KMDAddress   string `required:"true"`
-	KMDToken     string `required:"true"`
 
 	TokenAuthPassword string `required:"true"`
 
