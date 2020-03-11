ae02204445bdc0f16272c36262efc8dd817c51a1
Haardik <hhaardik@edu.uwaterloo.ca>
2020-03-11 00:13:24 -0400

    update base url to server

--- web/src/app/state.service.ts
+++ web/src/app/state.service.ts
@@ -11,31 +11,8 @@ import { AssetListing } from './asset-listing'
     providedIn: 'root',
 })
 export class StateService {
-    private baseURL = 'http://localhost:5000'
-
-    public currToken = new Token({
-        assetId: '212945',
-        creatorAddr:
-            '2LX7ZMR7SMDONF3FLD2SM5KUSKUWYKDH4WS76AW26US3Y3QB4Z4UROVFTY',
-        assetName: 'USD Tether',
-        unitName: 'USDT',
-        totalIssuance: 100000000,
-        decimals: 15,
-        defaultFrozen: false,
-        url: 'https://usdtether.io',
-        metadataHash: 'aldskfjlakjsdf;',
-        managerAddr:
-            '2LX7ZMR7SMDONF3FLD2SM5KUSKUWYKDH4WS76AW26US3Y3QB4Z4UROVFTY',
-        reserveAddr:
-            '2LX7ZMR7SMDONF3FLD2SM5KUSKUWYKDH4WS76AW26US3Y3QB4Z4UROVFTY',
-        freezeAddr:
-            '2LX7ZMR7SMDONF3FLD2SM5KUSKUWYKDH4WS76AW26US3Y3QB4Z4UROVFTY',
-        clawbackAddr:
-            '2LX7ZMR7SMDONF3FLD2SM5KUSKUWYKDH4WS76AW26US3Y3QB4Z4UROVFTY',
-        permissions: ['manager', 'clawback', 'freeze'],
-    })
-
-    public tokenList: Token[] = [this.currToken, this.currToken, this.currToken]
+    //private baseURL = 'http://localhost:5000'
+    private baseURL = 'http://api.mangodev.io'
 
     private authToken: string
 
