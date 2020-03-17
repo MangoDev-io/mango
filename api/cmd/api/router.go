a5f2b72b7ee879102591f09fc9e357fc62fbb11e
Haardik <hhaardik@edu.uwaterloo.ca>
2020-03-16 23:11:40 -0400

    update cors

--- api/cmd/api/router.go
+++ api/cmd/api/router.go
@@ -27,7 +27,7 @@ func NewRouterService(logger *logrus.Logger, db *data.DatabaseService, algod *al
 
 	cors := cors.New(cors.Options{
 		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
+		AllowedOrigins: []string{"https://mangodev.io", "https://www.mangodev.io"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
